673bba26f773b59f24410cee570ff390f2b1bec6
Rootkitcode <coaldar85@gmail.com>
2022-05-09 13:47:18 -0500

    nuevos ejemplos en objetos

--- curso_js/objetos/js/objects.js
+++ curso_js/objetos/js/objects.js
@@ -8,13 +8,16 @@
 
 let usuario = { //objeto literal
     name: "Darwin", //en la clave "name" se almacena el valor "Darwin"
-    edad: 36 //en la clave
+    edad: 36, //en la clave
+    "likes birds": true, //en la clave "likes birds" se almacena el valor true
     
     
 };
 console.log(usuario.name); //Darwin
 console.log(usuario.edad); //36
 console.log(usuario.admin = true); //true
+// console.log(delete usuario.edad); //true
+// console.log(usuario.likes birds = true); //true esto dara error
 
 //ejemplo 2
 
@@ -52,5 +55,20 @@ let key = prompt("¿que te gustaria saber acerca de las aves?", "alias");
 //tenemos acceso por medio de una variable
 console.log( animal[key] ); //pajaro
 
+//propiedades calculadas
+
+let fruta = prompt("Que fruta comprar?", "manzana");
+let bag = {
+    [fruta]: 8,
+};
+console.log(bag.apple); //8 si fruta es apple
 
 
+//la propiedad in
+
+let unos = {
+    name: "alguien",
+    edad: 25
+};
+console.log("edad" in unos); //true si existe la clave "edad"
+console.log("otra" in unos); //false si no existe la clave "otra en unos"
